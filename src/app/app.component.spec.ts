8b07a6a9864a46ab22bebd19ac285b7b79d4807c
Blaise Fombutu <blaise.fombutu@codenground.de>
2022-06-09 11:11:11 +0200

    todays commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'shopping-recipe-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shopping-recipe-app');
   });
 
   it('should render title', () => {
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'this is simple a test recipe for cooking nothing', 'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg' ),
+    new Recipe('slow roasted butter eggplant curry recipe', 'Inspired by the family favourite butter chicken, this vegie version slow-cooks the eggplant until', 'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg' )
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
