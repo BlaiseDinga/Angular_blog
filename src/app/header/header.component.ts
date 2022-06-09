5bc14e51f866e446b2d52fd581918e7bd8c79109
Blaise Fombutu <blaise.fombutu@codenground.de>
2022-06-09 11:21:51 +0200

    fixed error

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent{
-  @Output() featureSelected = new *<string>();
+  @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string){
     this.featureSelected.emit(feature);
